66ec8e45a46fd97193975096c70abb602e68b16e
sostinewaliaula <83455882+sostinewaliaula@users.noreply.github.com>
2025-09-01 09:31:23 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     allowedHosts: [
-      'dns.turnkey.local'
+      'it.internal.services'
     ],
     proxy: {
       '/api': {
